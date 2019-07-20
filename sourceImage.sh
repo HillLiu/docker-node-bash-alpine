c4832df8a759b7dbff7848b1250aff0688b218b0
Hill <hill@kimo.com>
2019-07-20 17:57:26 +0000

    update script

--- sourceImage.sh
+++ sourceImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z $sourceImage ]; then
-sourceImage=$(awk -F "=" '/sourceImage/ {print $2}' .env)
+sourceImage=$(awk -F "=" '/^sourceImage/ {print $2}' .env)
 fi
 
 echo $sourceImage
